09d6d1a354b08e74c2ced9847792b566594a3c82
Igor Rozen <sir@nabistudios.com>
2024-01-18 18:07:05 +0400

    Update menu_main.lua
    Version bump

--- script/system/menu_main.lua
+++ script/system/menu_main.lua
@@ -51,7 +51,7 @@ if (get_option("newmenu") == 0) then
 	return
 end
 
-TBMenu.Init("240115")
+TBMenu.Init("240118")
 require("system.menu_backend_defines")
 require("system.network_request")
 require("system.downloader_manager")
